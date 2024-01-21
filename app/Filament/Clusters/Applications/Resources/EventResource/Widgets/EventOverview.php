c2b9b2de8088c957aed4ec1968612d3635c94eef
fuzoh <info@bastiennicoud.ch>
2024-01-21 18:51:13 +0100

    feat(events): Little event trend chart
    Signed-off-by: fuzoh <info@bastiennicoud.ch>

--- app/Filament/Clusters/Applications/Resources/EventResource/Widgets/EventOverview.php
+++ app/Filament/Clusters/Applications/Resources/EventResource/Widgets/EventOverview.php
@@ -5,17 +5,27 @@ namespace App\Filament\Clusters\Applications\Resources\EventResource\Widgets;
 use App\Models\Event;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Flowframe\Trend\Trend;
 
 class EventOverview extends BaseWidget
 {
     protected function getStats(): array
     {
-        $event = Event::count();
+        $event = Trend::model(Event::class)
+            ->between(
+                start: now()->startOfWeek(),
+                end: now()->endOfDay()
+            )
+            ->perDay()
+            ->count();
+        $event = $event->map(fn ($value) => $value->aggregate);
+
         return [
-            Stat::make('Events', $event)
+            Stat::make('Événements', $event->sum())
                 ->description("Nombre d'événements dans le système")
                 ->descriptionIcon('heroicon-o-calendar')
-                ->color('info'),
+                ->color('info')
+                ->chart($event->toArray()),
         ];
     }
 }
