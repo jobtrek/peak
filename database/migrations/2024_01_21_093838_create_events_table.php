0632a8b363965ce54a0daac0c1f8a3cc9d2617a3
fuzoh <info@bastiennicoud.ch>
2024-01-21 10:59:15 +0100

    feat(events): Add models and migration for event and migration
    Signed-off-by: fuzoh <info@bastiennicoud.ch>

---
+++ database/migrations/2024_01_21_093838_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 230);
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
+            $table->foreignUuid('creator_id')->constrained('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
